0eb6b483ddca4f2315e03b2524b48b6bed366530
Mari023 <Mari_023@outlook.de>
2020-09-09 19:56:00 +0200

    implement Energy Storage

--- src/main/java/de/mari_023/fabric/marisultimatenetworks/blockentity/EnergyOutputBlockEntity.java
+++ src/main/java/de/mari_023/fabric/marisultimatenetworks/blockentity/EnergyOutputBlockEntity.java
@@ -2,9 +2,31 @@ package de.mari_023.fabric.marisultimatenetworks.blockentity;
 
 import de.mari_023.fabric.marisultimatenetworks.MarisUltimateNetworks;
 import net.minecraft.block.entity.BlockEntity;
+import team.reborn.energy.EnergySide;
+import team.reborn.energy.EnergyStorage;
+import team.reborn.energy.EnergyTier;
 
-public class EnergyOutputBlockEntity extends BlockEntity {
+public class EnergyOutputBlockEntity extends BlockEntity implements EnergyStorage {
     public EnergyOutputBlockEntity() {
         super(MarisUltimateNetworks.EnergyOutputBlockEntity);
+    }
+
+    @Override
+    public double getStored(EnergySide energySide) {
+        return 0;
+    }
+
+    @Override
+    public void setStored(double v) {
+    }
+
+    @Override
+    public double getMaxStoredPower() {
+        return Double.MAX_VALUE;
+    }
+
+    @Override
+    public EnergyTier getTier() {
+        return EnergyTier.INFINITE;
     }
 }
