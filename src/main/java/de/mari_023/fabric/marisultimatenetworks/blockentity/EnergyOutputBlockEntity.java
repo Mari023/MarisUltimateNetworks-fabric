108145dcbc0ab69953ac2784c50cbf9d57d6df45
Mari023 <Mari_023@outlook.de>
2020-09-10 19:22:49 +0200

    Fixed EnergyOutputBlock not Outputting Energy

--- src/main/java/de/mari_023/fabric/marisultimatenetworks/blockentity/EnergyOutputBlockEntity.java
+++ src/main/java/de/mari_023/fabric/marisultimatenetworks/blockentity/EnergyOutputBlockEntity.java
@@ -2,11 +2,14 @@ package de.mari_023.fabric.marisultimatenetworks.blockentity;
 
 import de.mari_023.fabric.marisultimatenetworks.MarisUltimateNetworks;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.util.Tickable;
+import net.minecraft.util.math.Direction;
+import team.reborn.energy.Energy;
 import team.reborn.energy.EnergySide;
 import team.reborn.energy.EnergyStorage;
 import team.reborn.energy.EnergyTier;
 
-public class EnergyOutputBlockEntity extends BlockEntity implements EnergyStorage {
+public class EnergyOutputBlockEntity extends BlockEntity implements EnergyStorage, Tickable {
     public EnergyOutputBlockEntity() {
         super(MarisUltimateNetworks.EnergyOutputBlockEntity);
     }
@@ -28,5 +31,23 @@ public class EnergyOutputBlockEntity extends BlockEntity implements EnergyStorag
     @Override
     public EnergyTier getTier() {
         return EnergyTier.INFINITE;
+    }
+
+    public void tick() {
+        if (getWorld() == null || getWorld().isClient) {
+            return;
+        }
+        for (Direction side : Direction.values()) {
+            BlockEntity blockEntity = getWorld().getBlockEntity(getPos().offset(side));
+            if (blockEntity == null || !Energy.valid(blockEntity)) {
+                continue;
+            }
+            Energy.of(this)
+                    .side(side)
+                    .into(
+                            Energy.of(blockEntity).side(side.getOpposite())
+                    )
+                    .move();
+        }
     }
 }
