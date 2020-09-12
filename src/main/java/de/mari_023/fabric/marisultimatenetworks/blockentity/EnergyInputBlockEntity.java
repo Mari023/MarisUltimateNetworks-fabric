961b02c68f31f7069dc67a28d71327e49480c336
Mari023 <Mari_023@outlook.de>
2020-09-12 16:07:52 +0200

    Refactored BlockEntities to have a common BasicBlockEntity

--- src/main/java/de/mari_023/fabric/marisultimatenetworks/blockentity/EnergyInputBlockEntity.java
+++ src/main/java/de/mari_023/fabric/marisultimatenetworks/blockentity/EnergyInputBlockEntity.java
@@ -1,32 +1,14 @@
 package de.mari_023.fabric.marisultimatenetworks.blockentity;
 
 import de.mari_023.fabric.marisultimatenetworks.MarisUltimateNetworks;
-import net.minecraft.block.entity.BlockEntity;
 import team.reborn.energy.EnergySide;
-import team.reborn.energy.EnergyStorage;
-import team.reborn.energy.EnergyTier;
 
-public class EnergyInputBlockEntity extends BlockEntity implements EnergyStorage {
+public class EnergyInputBlockEntity extends BlockEntityBasic {
     public EnergyInputBlockEntity() {
         super(MarisUltimateNetworks.EnergyInputBlockEntity);
     }
 
-    @Override
     public double getStored(EnergySide energySide) {
-        return Double.MAX_VALUE;
-    }
-
-    @Override
-    public void setStored(double v) {
-    }
-
-    @Override
-    public double getMaxStoredPower() {
-        return Double.MAX_VALUE;
-    }
-
-    @Override
-    public EnergyTier getTier() {
-        return EnergyTier.INFINITE;
+        return 0;
     }
 }
