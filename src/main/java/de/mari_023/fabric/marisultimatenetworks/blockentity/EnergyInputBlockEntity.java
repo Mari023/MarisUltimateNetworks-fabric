1033009b7335b6fdde5fa83c864cc0203e986fb2
Mari023 <Mari_023@outlook.de>
2020-09-12 17:42:31 +0200

    Revert "Refactored BlockEntities to have a common BasicBlockEntity"
    This reverts commit 961b02c6

--- src/main/java/de/mari_023/fabric/marisultimatenetworks/blockentity/EnergyInputBlockEntity.java
+++ src/main/java/de/mari_023/fabric/marisultimatenetworks/blockentity/EnergyInputBlockEntity.java
@@ -1,14 +1,32 @@
 package de.mari_023.fabric.marisultimatenetworks.blockentity;
 
 import de.mari_023.fabric.marisultimatenetworks.MarisUltimateNetworks;
+import net.minecraft.block.entity.BlockEntity;
 import team.reborn.energy.EnergySide;
+import team.reborn.energy.EnergyStorage;
+import team.reborn.energy.EnergyTier;
 
-public class EnergyInputBlockEntity extends BlockEntityBasic {
+public class EnergyInputBlockEntity extends BlockEntity implements EnergyStorage {
     public EnergyInputBlockEntity() {
         super(MarisUltimateNetworks.EnergyInputBlockEntity);
     }
 
+    @Override
     public double getStored(EnergySide energySide) {
-        return 0;
+        return Double.MAX_VALUE;
+    }
+
+    @Override
+    public void setStored(double v) {
+    }
+
+    @Override
+    public double getMaxStoredPower() {
+        return Double.MAX_VALUE;
+    }
+
+    @Override
+    public EnergyTier getTier() {
+        return EnergyTier.INFINITE;
     }
 }
--- src/main/java/de/mari_023/fabric/marisultimatenetworks/blockentity/EnergyOutputBlockEntity.java
+++ src/main/java/de/mari_023/fabric/marisultimatenetworks/blockentity/EnergyOutputBlockEntity.java
@@ -6,15 +6,31 @@ import net.minecraft.util.Tickable;
 import net.minecraft.util.math.Direction;
 import team.reborn.energy.Energy;
 import team.reborn.energy.EnergySide;
+import team.reborn.energy.EnergyStorage;
+import team.reborn.energy.EnergyTier;
 
-public class EnergyOutputBlockEntity extends BlockEntityBasic implements Tickable {
+public class EnergyOutputBlockEntity extends BlockEntity implements EnergyStorage, Tickable {
     public EnergyOutputBlockEntity() {
         super(MarisUltimateNetworks.EnergyOutputBlockEntity);
     }
 
     @Override
     public double getStored(EnergySide energySide) {
+        return 0;
+    }
+
+    @Override
+    public void setStored(double v) {
+    }
+
+    @Override
+    public double getMaxStoredPower() {
         return Double.MAX_VALUE;
+    }
+
+    @Override
+    public EnergyTier getTier() {
+        return EnergyTier.INFINITE;
     }
 
     public void tick() {
